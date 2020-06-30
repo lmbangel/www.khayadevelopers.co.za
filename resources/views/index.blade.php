0c77974178a8fbd0c5b796bdb0e222efd5976c2e
Lihle <mbangelalihle@gmail.com>
2020-06-30 13:18:14 +0200

    catered for https css refferencing

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Khayalami Developers</title>
         {{-- Styles --}}
-        <link rel="stylesheet" href="{{ URL::secure_asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 
         {{-- Scripts --}}
